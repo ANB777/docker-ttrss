8add7de5f1f0c6973f5bde57a7b14cc9ada82dff
ANB777 <anb@txtng.eu>
2019-02-17 15:59:24 +0100

    add ENV variables allowed to set via docker ENV

--- root/srv/ttrss-configure.php
+++ root/srv/ttrss-configure.php
@@ -7,12 +7,33 @@ if (!env('TTRSS_PATH', ''))
     $confpath = '/var/www/ttrss/';
 $conffile = $confpath . 'config.php';
 
-$available_env = [
-
+$allowed_env = [
+    'SINGLE_USER_MODE',
+    'SIMPLE_UPDATE_MODE',
+    'LOCK_DIRECTORY',
+    'CACHE_DIR',
+    'ICONS_DIR',
+    'ICONS_URL',
+    'AUTH_AUTO_CREATE',
+    'AUTH_AUTO_LOGIN',
+    'FORCE_ARTICLE_PURGE',
+    'SPHINX_SERVER',
+    'SPHINX_INDEX',
+    'ENABLE_REGISTRATION',
+    'REG_NOTIFY_ADDRESS',
+    'REG_MAX_USERS',
+    'SESSION_COOKIE_LIFETIME',
+    'SMTP_FROM_NAME',
+    'SMTP_FROM_ADDRESS',
+    'DIGEST_SUBJECT',
+    'CHECK_FOR_UPDATES',
+    'ENABLE_GZIP_OUTPUT',
+    'PLUGINS',
+    'LOG_DESTINATION',
 ];
 
 $config = array();
-foreach ($available_env as $env_name) {
+foreach ($allowed_env as $env_name) {
     // check if ENV variable exis. if they do use them
     if (!!env($env_name, '')) {
         $config[$env_name] = env($env_name);
